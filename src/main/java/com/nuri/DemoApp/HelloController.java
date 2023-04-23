d96b762f2bb2536f0fad1144910819cd76a5b046
‘mehmetnuricetin’ <mehmetnuricetin@gmail.com>
2023-04-23 18:37:17 +0200

    test

--- src/main/java/com/nuri/DemoApp/HelloController.java
+++ src/main/java/com/nuri/DemoApp/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, " + appenv + " new world!";
+        return "Hello, " + appenv + " new testing world!";
     }
 }
